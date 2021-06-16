7a956667b0b97fe1ef39b3bf8dc9d57dc6b085e9
Radu <radu.bordei89@gmail.com>
2021-06-16 18:42:37 +0300

    analytics improves.

--- app/Http/Controllers/API/AnalyticsController.php
+++ app/Http/Controllers/API/AnalyticsController.php
@@ -21,11 +21,19 @@ class AnalyticsController extends Controller {
 
         $reportsService = app(\App\Services\ReportsService::class);
 
-        $start_at = $validated['start_at'] ?? now()->copy()->startOfYear()->format('Y-m-d');
-        $end_at = $validated['end_at'] ?? now()->copy()->endOfYear()->format('Y-m-d');
+        // if not start_at, end_at set than use start of year
+        $start_at = !empty($validated['start_at']) ? Carbon::createFromFormat('Y-m-d',  $validated['start_at']) : now()->copy()->startOfYear();
+        $end_at = !empty($validated['end_at']) ? Carbon::createFromFormat('Y-m-d', $validated['end_at']) : now()->copy()->endOfYear();
         
-        $start_of_date = Carbon::createFromFormat('Y-m-d', $start_at);
-        $end_of_date = Carbon::createFromFormat('Y-m-d', $end_at);
+        $groupBy = isset($validated['groupBy']) ? $validated['groupBy'] : false;
+
+        // set group by param
+        if(!$groupBy) {
+            //TODO if range is bigger than a week set range to days
+            //TODO if range is bigger than a month set range to weeks
+            //TODO if range is bigger than a 3 to 6 months set rante to months
+            //TODO if range is bigger than 6 months set range to months
+        }
 
         //TODO check if manager through employeer get access to employees
         $access_to_employees = \Auth::user()->employees;
@@ -33,14 +41,40 @@ class AnalyticsController extends Controller {
         $employer = \Auth::user();
 
         $prev_period_data = $reportsService->getProductivityAnalytics(
-            $employer->id, $employee_ids->keys()->toArray(), $start_of_date, $end_of_date
+            $employer->id, $employee_ids->keys()->toArray(), $start_at, $end_at
         );
 
         $current_period_data = $reportsService->getProductivityAnalytics(
-            $employer->id, $employee_ids->keys()->toArray(), $start_of_date, $end_of_date
+            $employer->id, $employee_ids->keys()->toArray(), $start_at, $end_at
         );
 
+        // calculate current metrics
+        $current_productive_secs = $current_period_data->pluck('productive_secs')->sum();
+        $current_non_productive_secs = $current_period_data->pluck('non_productive_secs')->sum();
+        $current_neutral_secs = $current_period_data->pluck('neutral_secs')->sum();
+        $current_total_secs = $current_productive_secs + $current_non_productive_secs + $current_neutral_secs;
+
+
+         // calculate prev metrics
+         $prev_productive_secs = $prev_period_data->pluck('productive_secs')->sum();
+         $prev_non_productive_secs = $prev_period_data->pluck('non_productive_secs')->sum();
+         $prev_neutral_secs = $prev_period_data->pluck('neutral_secs')->sum();
+         $prev_total_secs = $prev_productive_secs + $prev_non_productive_secs + $prev_neutral_secs;
+
         return response()->json([
+            'total_secs' => [
+                'current' => $current_total_secs,
+                'prev' => $prev_total_secs,
+            ],
+            'productive_secs' => [
+                'current' => $current_productive_secs,
+                'prev' => $prev_productive_secs,
+            ],
+            'non_productive_secs' => [
+                'current' => $current_non_productive_secs,
+                'prev' => $prev_non_productive_secs,
+            ],
+
             'categories' => $current_period_data->pluck('month'),
             'prev_period_data' => [
                 'productive_secs' => $prev_period_data->pluck('productive_secs'),
@@ -53,6 +87,18 @@ class AnalyticsController extends Controller {
                 'non_productive_secs' => $prev_period_data->pluck('non_productive_secs'),
             ],
         ]);
+    }
+
+    public function employees(Request $request) {
+
+    }
+
+    public function popularApps(Request $request) {
+
+    }
+
+    public function popularCategories(Request $request) {
+
     }
 
     public function burnout(Request $request) {
--- helpers.php
+++ helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 if(! function_exists('get_name_from_email')) {
     function get_name_from_email($email_address, $split='@') {
         return ucwords(strtolower(substr($email_address, 0, strripos($email_address, $split))));
@@ -12,5 +14,50 @@ if(! function_exists('get_domain_from_email')) {
         $email_domain = $email_domain[1];
 
         return $email_domain;
+    }
+}
+
+if(! function_exists('get_dates_by_interval')) {
+
+    function get_dates_by_interval($interval = 'daily', $start_date) {
+        if ($interval == 'weekly') {
+            $start_of_date = $start_date->startOfWeek();
+            $end_of_date = $start_of_date->copy()->endOfWeek();
+        } elseif ($interval == 'monthly')  {
+            $start_of_date = $start_date->startOfMonth();
+            $end_of_date = $start_of_date->copy()->endOfMonth();
+        } elseif( $interval == 'yearly' ) {
+            $start_of_date = $start_date->startOfYear();
+            $end_of_date = $start_of_date->copy()->endOfYear();
+        } else {
+            $start_of_date = $start_date;
+            $end_of_date = $start_of_date;
+        }
+
+        return [$start_of_date, $end_of_date];
+    }
+}
+
+if(! function_exists('generate_date_range')) {
+    function generate_date_range(Carbon $start_date, Carbon $end_date, $group = 'day') {
+        $dates = [];
+
+        if($group == 'day') {
+            $formatter = 'Y-m-d';
+        } elseif ( $group == 'week' ) {
+            $formatter = 'Y/W';
+        } elseif ( $group == 'month' ) {
+            $formatter = 'Y-m';
+        } else {
+            $formatter = 'Y';
+        }
+
+        for($date = $start_date->copy(); $date->lte($end_date); $date->addDay()) {
+            if(! in_array($date->format($formatter), $dates)) {
+                $dates[] = $date->format($formatter);
+            }
+        }
+
+        return $dates;
     }
 }
