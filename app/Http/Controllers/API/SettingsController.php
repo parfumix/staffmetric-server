04edf3997ce8a37e3c84bcc1c8c0838d67770d20
Radu <radu.bordei89@gmail.com>
2021-06-18 18:21:01 +0300

    no message

--- app/Http/Controllers/API/SettingsController.php
+++ app/Http/Controllers/API/SettingsController.php
@@ -7,11 +7,5 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller {
     
-    public function applications(Request $request) {
-
-    }
-
-    public function categories(Request $request) {
-
-    }
+ 
 }
