2ae77810a6b66505330cbe725f705a2fe44a513f
Radu <radu.bordei89@gmail.com>
2021-06-18 14:03:09 +0300

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,17 +22,22 @@ class Kernel extends ConsoleKernel {
      * @return void
      */
     protected function schedule(Schedule $schedule) {
+        
+        // notify if uncategorized apps
         $schedule->command('staffmetric:uncategorized')
             ->weekly()->at('09:00');
             
+        // on hourly basis update analytics table
         $schedule->command('staffmetric:analytics')
             ->hourly()
             ->appendOutputTo( storage_path('logs/analytics.log') );
 
+        // calculate top apps each day
         $schedule->command('staffmetric:top_apps')
             ->daily()
             ->appendOutputTo( storage_path('top_apps.log') );
 
+        // each day add new apps to apps table
         $schedule->command('staffmetric:apps')
             ->daily()
             ->appendOutputTo( storage_path('apps.log') );
